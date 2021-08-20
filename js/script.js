134c638244c72f37d9ee2a88990de75583afe8aa
VADYA <joinlavrynovych@gmail.com>
2021-08-20 17:37:58 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+console.log('arr' + " - object");
+
+
+let incr = 10;
+let decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+
+const isChecked = true;
+const isClose = false;
+
+console.log(isChecked || isClose); 
